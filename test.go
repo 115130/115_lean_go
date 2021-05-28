775f79494d680d8c339ca3901e2ff7ba6b32f5f2
wodeq <wodeqiyuxin@gmail.com>
2021-05-28 21:51:16 +0800

    测试完毕

--- test.go
+++ test.go
@@ -2,17 +2,31 @@ package main
 
 const englishHelloPrefix = "Hello,"
 const chineseHelloPrefix = "你好,"
+const frenchHelloPrefix = "Bonjour,"
+const chinese = "chinese"
+const french = "french"
 
 // func main() {
 // 	fmt.Println(miao("miao"))
 // }
 
-func miao(name string, language string) string {
+func Miao(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "chinese" {
-		return chineseHelloPrefix + name
+	prefix := greetingPrefix(language)
+
+	return prefix + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case chinese:
+		prefix = chineseHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return
 }
