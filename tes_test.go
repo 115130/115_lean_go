5ecb80e67dea3cd74551619f295b39534b59b0d0
wodeq <wodeqiyuxin@gmail.com>
2021-05-28 21:22:08 +0800

    使用子测试

--- tes_test.go
+++ tes_test.go
@@ -3,9 +3,19 @@ package main
 import "testing"
 
 func TestMiao(t *testing.T) {
-	got := miao()
-	want := "miao"
-	if got != want {
-		t.Errorf("got是 '%q' want是 '%q123'", got, want)
-	}
+	t.Run("saying hello people", func(t *testing.T) {
+		got := miao("chris")
+		want := "Hello,chris"
+		if got != want {
+			t.Errorf("got '%q' , want '%q'", got, want)
+		}
+	})
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := miao("")
+		want := "Hello,World"
+
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
 }
