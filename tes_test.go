46cafbafd96a832c9cb26efb638767babf6f3dfa
wodeq <wodeqiyuxin@gmail.com>
2021-05-28 21:28:44 +0800

    使用函数重构

--- tes_test.go
+++ tes_test.go
@@ -1,21 +1,25 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMiao(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+
 	t.Run("saying hello people", func(t *testing.T) {
 		got := miao("chris")
 		want := "Hello,chris"
-		if got != want {
-			t.Errorf("got '%q' , want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
 		got := miao("")
 		want := "Hello,World"
-
-		if got != want {
-			t.Errorf("got '%q' want '%q'", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
